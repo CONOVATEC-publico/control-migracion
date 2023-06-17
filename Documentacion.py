82bd96ad20a9d996e1783e0051f40338574a5cef
Neyberson <neyberatencio@gmail.com>
2023-06-17 17:11:35 -0500

    Cambiando Print por Input en las funciones set_

--- Documentacion.py
+++ Documentacion.py
@@ -1,5 +1,7 @@
 class Usuario:
 
+
+
     def __init__(self,tipoUsuario,numeroPasaporte,numeroIdentificacion,nombreCompleto,anhoNacimiento,paisOrigen,nombreContactoPaisOrigen,nombreContactoPaisDestino,telefonoContactoPaisOrigen,telefonoContactoPaisDestino,reservaLugarDestino,direccionLugarDestino,boletoSalida):
         self.tipoUsuario = tipoUsuario
         self.numeroPasaporte = numeroPasaporte
@@ -43,29 +45,29 @@ class Usuario:
 
 #Se definen las condiciones para la digitación de los datos del Usuario
     def set_tipoUsuario (self):
-        print(f"Digite su tipo usuario: {self.tipoUsuario}")
+        input(f"Digite su tipo usuario: {self.tipoUsuario}")
     def set_numeroPasaporte (self):
-        print(f"Digite su número de pasaporte: {self.numeroPasaporte}")
+        input(f"Digite su número de pasaporte: {self.numeroPasaporte}")
     def set_numeroIdentificacion (self):
-        print(f"Digite el número de identificación del documento de su País de origen{self.numeroIdentificacion}")
+        input(f"Digite el número de identificación del documento de su País de origen{self.numeroIdentificacion}")
     def set_anhoNacimiento (self):
-        print(f"Digite su año de nacimiento: {self.anhoNacimiento}")
+        input(f"Digite su año de nacimiento: {self.anhoNacimiento}")
     def set_paisOrigen (self):
-        print(f"Digite su país de origen: {self.paisOrigen}")
+        input(f"Digite su país de origen: {self.paisOrigen}")
     def set_nombreContactoPaisOrigen (self):
-        print(f"Digite el nombre completo de una persona que respalde su identidad en su país de origen, de preferencia un familiar: {self.nombreContactoPaisOrigen}")
+        input(f"Digite el nombre completo de una persona que respalde su identidad en su país de origen, de preferencia un familiar: {self.nombreContactoPaisOrigen}")
     def set_telefonoContactoPaisOrigen (self):
-        print(f"Digite el número de la persona que respalde su identidad en su país de origen: {self.telefonoContactoPaisOrigen}")
+        input(f"Digite el número de la persona que respalde su identidad en su país de origen: {self.telefonoContactoPaisOrigen}")
     def set_nombreContactoPaisDestino (self):
-        print(f"Digite el nombre completo de una persona que respalde su identidad en el país de destino que se dirige, de preferencia un familiar: {self.nombreContactoPaisDestino}")
+        input(f"Digite el nombre completo de una persona que respalde su identidad en el país de destino que se dirige, de preferencia un familiar: {self.nombreContactoPaisDestino}")
     def set_telefonoContactoPaisDestino (self):
-        print(f"El número de teléfono de la persona que respalda su identidad en el país de su destino: {self.telefonoContactoPaisDestino}")
+        input(f"El número de teléfono de la persona que respalda su identidad en el país de su destino: {self.telefonoContactoPaisDestino}")
     def set_reservaLugarDestino (self):
-        print(f"Digite el código de la reserva que tiene hecha en el lugar en donde se hospedará, si esta es un familiar escribir la misma palabra y si es otra particular escribir también dicha palabra: {self.reservaLugarDestino}")
+        input(f"Digite el código de la reserva que tiene hecha en el lugar en donde se hospedará, si esta es un familiar escribir la misma palabra y si es otra particular escribir también dicha palabra: {self.reservaLugarDestino}")
     def set_direccionLugarDestino (self):
-        print(f"Digite la dirección del lugar en donde se hospedará, el orden debe ser Ciudad, Barrio, Dirección: {self.direccionLugarDestino}")
+        input(f"Digite la dirección del lugar en donde se hospedará, el orden debe ser Ciudad, Barrio, Dirección: {self.direccionLugarDestino}")
     def set_boletoSalida (self):
-        print(f"Digite el código del boleto que tomó ya sea de avión, barco o autobus: {self.boletoSalida}")
+        input(f"Digite el código del boleto que tomó ya sea de avión, barco o autobus: {self.boletoSalida}")
         
         
 Usuario.set_tipoUsuario()
